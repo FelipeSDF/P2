e960e927716c3d5da4d546c03ff980156f7de51a
Felipe Sodre de Freitas <162763118+FelipeSDF@users.noreply.github.com>
2024-11-29 19:40:21 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,21 +24,3 @@ function start_carrossel() {
 function parar_carrossel() {
     clearInterval(interval);
 }
-// start_carrossel()
-
-// const box = document.querySelector(".container");
-// const imagens = document.querySelectorAll(".container img");
-
-// let contador = 0;
-
-// function slider() {
-//   contador++;
-
-//   if (contador > imagens.length - 1) {
-//     contador = 0;
-//   }
-
-//   box.style.transform = `translateX(${-contador * 1250}px)`;
-// }
-
-// setInterval(slider, 2000);
